78a762d2442ae8f806ba91cfb46f6ab4a2cb054e
MortenStaunstrup <mort490fgromling@gmail.com>
2024-11-13 19:19:18 +0100

    Bocs

--- box-vis.js
+++ box-vis.js
@@ -1,92 +1,96 @@
-const svg = d3.selectAll(".visual-container").append("svg").attr("width", 1122).attr("height", 302);
+const svg = d3.selectAll(".visual-container").append("svg").attr("width", 1560).attr("height", 1002);
 console.log(svg);
 
 
-// top of box
+function createBox (xShift, yShift, ScalingFactor) {
 
 svg.append('line')
-.attr('x1', 50)
-.attr('y1', 75)
-.attr('x2', 100)
-.attr('y2', 110)
-.attr('stroke', 'red');
+  .attr('x1', 156 * ScalingFactor + xShift)  
+  .attr('y1', 655 * ScalingFactor + yShift)  
+  .attr('x2', 312 * ScalingFactor + xShift) 
+  .attr('y2', 720 * ScalingFactor + yShift) 
+  .attr('stroke', 'red');
 
 svg.append('line')
-.attr('x1', 100)
-.attr('y1', 110)
-.attr('x2', 150)
-.attr('y2', 75)
-.attr('stroke', 'red');
+  .attr('x1', 312 * ScalingFactor  + xShift)
+  .attr('y1', 720 * ScalingFactor + yShift) 
+  .attr('x2', 468 * ScalingFactor  + xShift)
+  .attr('y2', 655 * ScalingFactor + yShift)  
+  .attr('stroke', 'red');
 
 svg.append('line')
-.attr('x1', 150)
-.attr('y1', 75)
-.attr('x2', 100)
-.attr('y2', 40)
-.attr('stroke', 'red');
+  .attr('x1', 468 * ScalingFactor  + xShift)
+  .attr('y1', 655 * ScalingFactor + yShift )  
+  .attr('x2', 312 * ScalingFactor  + xShift)
+  .attr('y2', 590 * ScalingFactor + yShift) 
+  .attr('stroke', 'red');
 
 svg.append('line')
-.attr('x1', 100)
-.attr('y1', 40)
-.attr('x2', 50)
-.attr('y2', 75)
-.attr('stroke', 'red');
+  .attr('x1', 312 * ScalingFactor  + xShift)
+  .attr('y1', 590 * ScalingFactor + yShift)  
+  .attr('x2', 156 * ScalingFactor  + xShift)
+  .attr('y2', 655 * ScalingFactor + yShift)  
+  .attr('stroke', 'red');
 
-
-// bottom of box
-
+// Bottom of box
 
 svg.append('line')
-.attr('x1', 50)
-.attr('y1', 145)
-.attr('x2', 100)
-.attr('y2', 180)
-.attr('stroke', 'red');
+  .attr('x1', 156 * ScalingFactor  + xShift)
+  .attr('y1', 865 * ScalingFactor + yShift)
+  .attr('x2', 312 * ScalingFactor  + xShift) 
+  .attr('y2', 925 * ScalingFactor  + yShift)  
+  .attr('stroke', 'red');
 
 svg.append('line')
-.attr('x1', 100)
-.attr('y1', 180)
-.attr('x2', 150)
-.attr('y2', 145)
-.attr('stroke', 'red');
+  .attr('x1', 312 * ScalingFactor  + xShift)
+  .attr('y1', 925 * ScalingFactor + yShift) 
+  .attr('x2', 468 * ScalingFactor  + xShift)
+  .attr('y2', 865 * ScalingFactor + yShift) 
+  .attr('stroke', 'red');
 
 svg.append('line')
-.attr('x1', 150)
-.attr('y1', 145)
-.attr('x2', 100)
-.attr('y2', 110)
-.attr('stroke', 'red');
+  .attr('x1', 468 * ScalingFactor  + xShift)
+  .attr('y1', 865 * ScalingFactor + yShift)  
+  .attr('x2', 312 * ScalingFactor  + xShift)
+  .attr('y2', 800 * ScalingFactor + yShift)  
+  .attr('stroke', 'red');
 
 svg.append('line')
-.attr('x1', 100)
-.attr('y1', 110)
-.attr('x2', 50)
-.attr('y2', 145)
-.attr('stroke', 'red');
-
+  .attr('x1', 312 * ScalingFactor  + xShift)
+  .attr('y1', 800 * ScalingFactor + yShift) 
+  .attr('x2', 156 * ScalingFactor  + xShift)
+  .attr('y2', 865 * ScalingFactor + yShift)  
+  .attr('stroke', 'red');
 
 // Sides of box
 
+svg.append('line')
+  .attr('x1', 156 * ScalingFactor  + xShift)
+  .attr('y1', 655 * ScalingFactor + yShift)  
+  .attr('x2', 156 * ScalingFactor  + xShift)
+  .attr('y2', 865 * ScalingFactor + yShift)  
+  .attr('stroke', 'red');
 
 svg.append('line')
-.attr('x1', 50)
-.attr('y1', 75)
-.attr('x2', 50)
-.attr('y2', 145)
-.attr('stroke', 'red');
-
-
-svg.append('line')
-.attr('x1', 150)
-.attr('y1', 75)
-.attr('x2', 150)
-.attr('y2', 145)
-.attr('stroke', 'red');
-
+  .attr('x1', 468 * ScalingFactor + xShift)
+  .attr('y1', 655 * ScalingFactor + yShift)  
+  .attr('x2', 468 * ScalingFactor + xShift)
+  .attr('y2', 865 * ScalingFactor + yShift) 
+  .attr('stroke', 'red');
 
 svg.append('line')
-.attr('x1', 100)
-.attr('y1', 110)
-.attr('x2', 100)
-.attr('y2', 180)
-.attr('stroke', 'red');
+  .attr('x1', 312 * ScalingFactor  + xShift)
+  .attr('y1', 720 * ScalingFactor + yShift) 
+  .attr('x2', 312 * ScalingFactor  + xShift) 
+  .attr('y2', 925 * ScalingFactor + yShift)  
+  .attr('stroke', 'red');
+
+  return this;
+}
+
+createBox(0, 500, 0.5);
+createBox(250, 500, 0.5);
+
+createBox(500, 500, 0.5);
+createBox(750, 500, 0.5);
+createBox(1000, 500, 0.5);
