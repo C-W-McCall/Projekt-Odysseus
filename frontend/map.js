c083c7e281b7c3b309ea3d8e394f827c45c11e21
C-W-McCall <elias@liebman.dk>
2024-11-21 14:52:05 +0100

    Ny kort

--- frontend/map.js
+++ frontend/map.js
@@ -1,10 +1,14 @@
 // Initialize the map and set its view to a specific location and zoom level
-const map = L.map('map').setView([56.1629, 10.2039], 7); // Starting at coordinates [0, 0] with a zoom of 2
+const map = L.map('map', {
+}).setView([56.1629, 10.2039], 7);
 
 // Add a tile layer to add to the map (using OpenStreetMap tiles)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    noWrap: true
 }).addTo(map);
+
+
 
 // Load GeoJSON data and add clustering for better performance
 fetch('Geo.geojson')
@@ -29,6 +33,8 @@ fetch('Geo.geojson')
             }
         });
         markers.addLayer(geoJsonLayer);
+
+        
 
         // Add the marker cluster group to the map
         map.addLayer(markers);
