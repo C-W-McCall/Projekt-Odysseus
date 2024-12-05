1edf82c14b983d4d0493f76d90c784257f453f3f
C-W-McCall <elias@liebman.dk>
2024-12-05 08:37:10 +0100

    ny

--- frontend/beskrivelse.js
+++ frontend/beskrivelse.js
@@ -1,7 +1,7 @@
 var typed = new Typed('#typed', {
-    strings: ['Hejsa!', 'Velkommen til vores side :)', 'Her deler vi indsigtsfulde data om microplast i verdenshavene!', 'Dette projekt er skabt for at give et klart og detaljeret overblik over omfanget af microplastforurening i havene verden over. Gennem en række visualiseringer kan du udforske data, der viser koncentrationer, fordeling og udvikling af microplast i forskellige områder af verdenshavene. Vores mål er at gøre dataen tilgængelig og let at forstå, så du kan få en bedre indsigt i, hvor meget microplast der findes i vores oceaner – og hvor det ophober sig. Tak fordi du kigger forbi. Vi håber, at vores visualiseringer kan bidrage til en større forståelse for dette vigtige emne.'],
-    typeSpeed: 25,
+    strings: ['Hejsa!', 'Velkommen til vores side :)', 'Dette projekt er skabt for at give et klart og detaljeret overblik over omfanget af microplastforurening i havene verden over. Gennem en række visualiseringer kan du udforske data, der viser koncentrationer, fordeling og udvikling af microplast i forskellige områder af verdenshavene. Vores mål er at gøre dataen tilgængelig og let at forstå, så du kan få en bedre indsigt i, hvor meget microplast der findes i vores oceaner – og hvor det ophober sig.', 'På nedenstående kort finder du hvor i verden vores målinger er taget. Prøv at tykke på en markør, så får du mere information om den enkelte måling. Tak fordi du kigger forbi.'],
+    typeSpeed: 20,
     startDelay: 500,
     backDelay: 1000,
-    backSpeed: 35,
+    backSpeed: 20,
 });
