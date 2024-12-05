e8c700eae92dd8b22de6fe3d0aff4cb11d82f67e
Thanushanb <Thanushanbs@gmail.com>
2024-12-05 09:46:07 +0100

    færdig

--- frontend/scrollbar.js
+++ frontend/scrollbar.js
@@ -1,10 +1,11 @@
 ScrollReveal().reveal(
     ".text-box-container, .visualisering-boks, .visuals, .buttns-viz, #chartContainer, .map-wrapper, #line-viz, .live-counter",
     {
-        delay: 700, // Lavere forsinkelse for hurtigere respons
-        duration: 800, // Glidende animation på 800ms
-        easing: "ease-in-out", // Glat acceleration og deceleration
-        interval: 300, // Kortere interval mellem elementer
-        scale: 1.7, // Subtil skalering
+        delay: 700,
+        duration: 800,
+        easing: "ease-in-out",
+        interval: 300,
+        scale: 1.1, // Mindre skalering
+        opacity: 0, // Starter usynligt
     }
 );
