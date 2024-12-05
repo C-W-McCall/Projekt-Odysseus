ef80f9f6ac909d0828f191d0baf6edaa5278c468
Thanushanb <Thanushanbs@gmail.com>
2024-12-05 08:51:43 +0100

    e

--- frontend/scrollbar.js
+++ frontend/scrollbar.js
@@ -1,12 +1,10 @@
 ScrollReveal().reveal(
     ".text-box-container, .visualisering-boks, .visuals, .buttns-viz, #chartContainer, .map-wrapper, #line-viz, .live-counter",
     {
-        delay: 300, // Lavere forsinkelse for hurtigere respons
+        delay: 700, // Lavere forsinkelse for hurtigere respons
         duration: 800, // Glidende animation på 800ms
         easing: "ease-in-out", // Glat acceleration og deceleration
         interval: 300, // Kortere interval mellem elementer
-        scale: 1.05, // Subtil skalering
-        viewOffset: { top: 200, right: 0, bottom: 200, left: 0 }, // Starter tættere på viewporten
-        reset: true // Elementer forsvinder og genanimeres, når de forlader viewporten
+        scale: 1.7, // Subtil skalering
     }
 );
