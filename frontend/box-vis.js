dcb9f9e4310aab02ee51b08d73a526f482ebfc89
MortenStaunstrup <mort490fgromling@gmail.com>
2024-12-06 19:21:18 +0100

    Lidt css igen

--- frontend/box-vis.js
+++ frontend/box-vis.js
@@ -1,4 +1,4 @@
-const svg = d3.selectAll(".visualisering-content").append("svg").attr("width", 1400).attr("height", 300);
+const svg = d3.selectAll(".visualisering-content").append("svg").attr("width", 1400).attr("height", 300).attr("id", "spandVizSvg"); 
 
 
 d3.json(`/api/albums`).then((data) => {
